df9ffe2f731a3c7f9aef2e85efd8de7fdd7049ff
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-10-19 18:18:33 +0300

    New Commit

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/smartedu-db' , {
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+  //useFindAndModify : false,
+  //useCreateIndex: true
+}).then(() => {
+  console.log('DB Connected Succesfully');
+});
 
 //Template Engine
 app.set("view engine", "ejs" );
 
 //Middleware
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 //Routes
 app.use('/', pageRoute );
+app.use('/courses', courseRoute );
 
-app.get('/about', );
+
 
 const port = 3000;
 app.listen(port, () => {
