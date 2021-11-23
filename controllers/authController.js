f9d266bf443113caeab74fed1a8ee9cf9d51858f
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-11-23 17:55:32 +0300

    Dasboard sayfası yapıldı

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ exports.loginUser =  (req, res) => {
           if (same) {
             // USER SESSION
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -47,3 +47,11 @@ exports.logoutUser = (req, res) => {
     res.redirect('/');
   })
 }
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id:req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
+  });
+}; 
