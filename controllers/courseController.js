3dead87c9b1f1f0800e62376bbb34b5c7f20bfdf
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-11-23 16:36:03 +0300

    Login işlemleri yapıldı

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -20,13 +20,12 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
-    const category = await Category.findOne({ slug : categorySlug })
+    const category = await Category.findOne({ slug: categorySlug });
 
     let filter = {};
-    if (categorySlug){
-      filter = {category: category._id}
+    if (categorySlug) {
+      filter = { category: category._id };
     }
-
 
     const courses = await Course.find(filter);
     const categories = await Category.find();
--- models/Course.js
+++ models/Course.js
@@ -20,20 +20,19 @@ const CourseSchema = new Schema({
   slug: {
     type: String,
     unique: true,
-
   },
   category: {
     type: mongoose.Schema.Types.ObjectId,
-    ref : 'Category'
-  }
+    ref: 'Category',
+  },
 });
 
-CourseSchema.pre('validate', function(next) {
+CourseSchema.pre('validate', function (next) {
   this.slug = slugify(this.name, {
-    lower : true,
-    strict : true,
-  })
+    lower: true,
+    strict: true,
+  });
   next();
-})
-const Course = mongoose.model('Course' , CourseSchema);
+});
+const Course = mongoose.model('Course', CourseSchema);
 module.exports = Course;
