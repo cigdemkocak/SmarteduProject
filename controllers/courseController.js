313ee6c2deea80d307e249326e2dd8fc19ea4db8
Çiğdem Koçak <kocakcigdem9@gmail.com>
2021-11-30 16:55:29 +0300

    Admin Sayfası

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -151,3 +151,4 @@ exports.updateCourse = async (req, res) => {
     });
   }
 };
+
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,12 +1,23 @@
 const nodemailer = require("nodemailer");
+const Course = require('../models/Course');
+const User = require('../models/User');
 
-exports.getIndexPage = (req, res) => {
-    console.log(req.session.userID);
-    res.status(200).render('index', {
-      page_name: 'index',
-    });
-  };
+
+exports.getIndexPage = async (req, res) => {
   
+  const courses = await Course.find().sort('-createdAt').limit(2);
+  const totalCourses = await Course.find().countDocuments();
+  const totalStudents = await User.countDocuments({role: 'student'});
+  const totalTeachers = await User.countDocuments({role: 'teacher'});
+
+  res.status(200).render('index', {
+    page_name: 'index',
+    courses,
+    totalCourses,
+    totalStudents,
+    totalTeachers
+  });
+};
 
 exports.getAboutPage = (req, res) => {
   res.status(200).render('about', {
@@ -33,9 +44,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-
-  try{
-  const outputMessage = `
+  try {
+    const outputMessage = `
   
   <h1>Mail Details </h1>
   <ul>
@@ -44,39 +54,38 @@ exports.sendEmail = async (req, res) => {
   </ul>
   <h1>Message</h1>
   <p>${req.body.message}</p>
-  `
+  `;
 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "kocakcigdem9@gmail.com", // gmail account
-      pass: "rtfnzkzzrrpxejes", // gmail password
-    },
-  });
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: 'kocakcigdem9@gmail.com', // gmail account
+        pass: 'rtfnzkzzrrpxejes', // gmail password
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart EDU Contact Form" <kocakcigdem@gmail.com>', // sender address
-    to: "kadriyekocak715@gmail.com", // list of receivers
-    subject: "Smart EDU Contact Form New Message ✔", // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <kocakcigdem@gmail.com>', // sender address
+      to: 'kadriyekocak715@gmail.com', // list of receivers
+      subject: 'Smart EDU Contact Form New Message ✔', // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  req.flash("success", "We Received your message succesfully");
-  res.status(200).redirect('contact');
-
-}catch(err){
-  //req.flash("error", `Something happened! ${err}` );
-  req.flash("error", `Something happened!` );
-  res.status(200).redirect('contact');
-}
+    req.flash('success', 'We Received your message succesfully');
+    res.status(200).redirect('contact');
+  } catch (err) {
+    //req.flash("error", `Something happened! ${err}` );
+    req.flash('error', `Something happened!`);
+    res.status(200).redirect('contact');
+  }
 };
